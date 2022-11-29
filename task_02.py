0f8d8de2b408713a34ffc60ad01009b1a23249c5
oman69 <kazyk-89@mail.ru>
2022-11-29 13:39:28 +0300

    Edit task_02, task_03

--- task_02.py
+++ task_02.py
@@ -2,7 +2,7 @@ def coincidence(my_list=None, my_range=None):
     if my_list and my_range:
         my_ls = []
         for item in my_list:
-            if isinstance(item, int | float):
+            if isinstance(item, (int, float)):
                 if my_range[0] <= item <= my_range[-1]:
                     my_ls.append(item)
         return my_ls
--- task_03.py
+++ task_03.py
@@ -1,5 +1,5 @@
 def max_odd(my_list):
-    my_list = [i for i in my_list if isinstance(i, int | float) and i % 2 == 1]
+    my_list = [i for i in my_list if isinstance(i, (int, float)) and i % 2 == 1]
     try:
         return int(max(my_list))
     except ValueError:
